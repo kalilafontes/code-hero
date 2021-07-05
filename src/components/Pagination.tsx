a781c7bd39efd195d5ce70e5df28f1ba85667723
c1318876 Kalila Maria Fontes Teixeira Cruz <c1318876@interno.bb.com.br>
2021-07-05 10:31:43 -0300

    Alteração na paginação

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -9,25 +9,35 @@ interface PaginationProps {
 
 const Pagination: React.FC<PaginationProps>= ({postsPerPage, totalPosts, paginate, currentPage}) => {
     const pageNumbers = [];
+    const lastPage = totalPosts/postsPerPage;
 
-    for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    for(let i = 1; i <= Math.ceil(lastPage); i++) {
         pageNumbers.push(i);
     }
-   
+    
+   console.log('postsPerPage', postsPerPage, 'totalPosts',totalPosts, 'currentPage', currentPage)
     return (
         <div>
-            <ul className="pagination">            
+            <ul className="pagination">  
+                <li className={currentPage == 1 ? 'disable-button': 'page-link'} >
+                    <button  onClick={() => paginate(1)}><i className="fa fa-chevron-left"></i><i className="fa fa-chevron-left"></i></button>
+                </li>    
+                <li className={currentPage == 1 ? 'disable-button': 'page-link'}>
+                    <button  onClick={() => paginate(currentPage-1)}><i className="fa fa-chevron-left"></i></button></li>          
                 {pageNumbers.map(number => (    
-                                   
+                                
                     <li key={number} className={currentPage === number ? 'pagination__item--active': 'page-link'}>
                         <a onClick={() => paginate(number)} href='#' >
                             {number}
                         </a>
-                        {console.log(postsPerPage, number) }
                     </li>                    
                 ))}
-                    
-
+                <li className={currentPage == lastPage ? 'disable-button': 'page-link'}>
+                    <button  onClick={() => paginate(currentPage+1)}><i className="fa fa-chevron-right"></i></button>
+                </li>
+                <li className={currentPage == lastPage ? 'disable-button': 'page-link'}>
+                    <button  onClick={() => paginate(lastPage)}><i className="fa fa-chevron-right"></i><i className="fa fa-chevron-right"></i></button>
+                </li>                
             </ul>
         </div>
     );
